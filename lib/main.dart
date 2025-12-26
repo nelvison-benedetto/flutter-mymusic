eab3f25634e2a9134a7a9af7a8f22104eaeb5e82
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-12-26 14:21:56 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:just_audio/just_audio.dart';
+
 void main(){ runApp( MainPage()); }
 
 class MainPage extends StatefulWidget{
@@ -10,6 +12,19 @@ class MainPage extends StatefulWidget{
 
 class _MainPageState extends State<MainPage>{
 
+  final AudioPlayer _player = AudioPlayer();
+
+  Future<void> playSound() async {
+    await _player.setAsset('assets/audio/mysound1.wav');
+    _player.play();
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -17,7 +32,12 @@ class _MainPageState extends State<MainPage>{
       home: Scaffold(
           appBar : AppBar(),
           body : SafeArea(
-            child:Container()
+            child: Center(
+              child: ElevatedButton(
+                onPressed: playSound,
+                child: const Text('Play Sound'),
+              ),
+            ),
           )
       ),
     );
